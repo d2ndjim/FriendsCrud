c6f477078dbf5ac42cb3a6f0d58e9c2d141a0257
Lekan <jimoridwanlekan@gmail.com>
2022-08-15 20:40:19 +0100

    Modify styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end
   resources :friends
-  root 'home#index'
+  # root 'home#index'
+  root 'friends#index'
   get '/home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
